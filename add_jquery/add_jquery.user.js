7b1bc53855cb95b466aad753ab68c6a169e1e4b6
Rahul Sarkar <rsarkar@sxiq.com.au>
2021-05-29 13:54:47 +1000

    refactoring the jquery loading script

--- add_jquery/add_jquery.user.js
+++ add_jquery/add_jquery.user.js
@@ -7,22 +7,52 @@
 // @match        https://*/*
 // @grant        none
 // ==/UserScript==
-(function () {
-    'use strict';
-    function addJQuery(callback) {
-        var script = document.createElement("script");
-        script.setAttribute("src", "https://code.jquery.com/jquery-3.5.1.min.js");
-        script.addEventListener('load', function () {
-            var script = document.createElement("script");
-            if(callback != undefined && callback != null){
-                script.textContent = "(" + callback.toString() + ")();";
-            }
-            document.body.appendChild(script);
-        }, false);
-        document.body.appendChild(script);
-    };
 
-    if (window.jQuery == undefined) {
-        addJQuery();
-    }
+window.userScript_taskPlanner = {
+  loadScript: function (url, sha, callback) {
+    //Adding jquery
+    var script = document.createElement("script");
+    script.setAttribute("src", url);
+    script.setAttribute("integrity", sha);
+    script.setAttribute("crossorigin", "anonymous");
+    script.addEventListener(
+      "load",
+      function () {
+        var script = document.createElement("script");
+        if (callback != undefined && callback) {
+          script.textContent = "(" + callback.toString() + ")();";
+        }
+        document.body.appendChild(script);
+      },
+      false
+    );
+    document.body.appendChild(script);
+    return script;
+  },
+
+  loadCss: function (url) {
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", url);
+    document.getElementsByTagName("head")[0].appendChild(fileref);
+  },
+};
+
+https: (function () {
+  "use strict";
+  function addJQuery(callback) {
+    //Adding jquery
+    userScript_taskPlanner.loadScript(
+      "https://code.jquery.com/jquery-3.6.0.min.js",
+      "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
+      callback
+    );
+  }
+
+  if (window.jQuery == undefined) {
+    addJQuery(function () {
+      console.log("jquery loaded");
+    });
+  }
 })();
